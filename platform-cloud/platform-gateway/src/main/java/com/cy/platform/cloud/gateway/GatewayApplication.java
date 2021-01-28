257d901effef9a82cf5f18a4912625d4b320e7eb
568074019@qq.com <568074019@qq.com>
2021-01-28 18:03:27 +0800

    使用Nacos作为配置中心代码提交

--- platform-cloud/platform-gateway/src/main/java/com/cy/platform/cloud/gateway/GatewayApplication.java
+++ platform-cloud/platform-gateway/src/main/java/com/cy/platform/cloud/gateway/GatewayApplication.java
@@ -3,6 +3,11 @@ package com.cy.platform.cloud.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ *
+ * @author develop
+ */
 @SpringBootApplication
 public class GatewayApplication {
     public static void main(String[] args) {
