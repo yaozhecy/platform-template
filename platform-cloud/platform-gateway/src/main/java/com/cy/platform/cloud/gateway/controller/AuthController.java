c9f6ee88a29167dea90dd567310ac30ae8e63ddf
568074019@qq.com <568074019@qq.com>
2021-03-11 15:39:26 +0800

    Nacos注册中心配置

--- platform-cloud/platform-gateway/src/main/java/com/cy/platform/cloud/gateway/controller/AuthController.java
+++ platform-cloud/platform-gateway/src/main/java/com/cy/platform/cloud/gateway/controller/AuthController.java
@@ -1,9 +1,10 @@
 package com.cy.platform.cloud.gateway.controller;
 
-import com.cy.platform.cloud.gateway.service.AuthService;
 import com.cy.platform.cloud.gateway.db.vo.AuthUserVo;
 import com.cy.platform.cloud.gateway.db.vo.LoginInfoReq;
+import com.cy.platform.cloud.gateway.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,10 +20,13 @@ import reactor.core.publisher.Mono;
 public class AuthController {
     @Autowired
     private AuthService authService;
+    @Value("${version}")
+    private String version;
 
     @PostMapping("/login")
     public Mono<String> login(Mono<LoginInfoReq> person) {
         String path = "/main/dashboard";
+        System.out.println(version);
         return Mono.create(monoSink -> monoSink.success(path));
     }
 
