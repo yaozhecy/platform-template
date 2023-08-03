d13b06be0746dafb9ca80dc9035969f317b770dc
chenyang <568074019@qq.com>
2023-08-03 21:53:14 +0800

    项目结构调整

---
+++ platform-server/platform-temp-provider/src/main/java/com/cy/platform/temp/server/TempApplication.java
@@ -0,0 +1,11 @@
+package com.cy.platform.temp.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TempApplication.class, args);
+    }
+}
---
+++ platform-server/platform-temp-provider/src/main/java/com/cy/platform/temp/server/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.cy.platform.temp.server.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public String test() {
+        System.out.println("success");
+        return "success";
+    }
+}
